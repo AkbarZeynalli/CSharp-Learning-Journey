637e29362425b5f5c8102e87848e7158b609a799
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-01-28 02:32:27 +0400

    day1 change

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello C#");
+﻿//string name = "Ek\0ber";
+//Console.WriteLine(name);
+//Console.WriteLine(name.Length); 
+
+
+
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
+//Console.WriteLine("\u00A9");
+//Console.WriteLine("\U0001F600");
+
+
+
+//int num1 = 7;
+//int num2 = 6;
+//if (num1 < num2)
+//{
+//    Console.ForegroundColor = ConsoleColor.Green;
+//    Console.WriteLine($"{num1} {num2} dan kiçikdir");
+//    Console.ResetColor();
+//}
+//else
+//{
+//    Console.ForegroundColor = ConsoleColor.Red;
+//    Console.WriteLine($"{num1} {num2} dan böyükdür");
+//    Console.ResetColor();
+//}
+
+
+//C# Variables and Constants
+//const int num = 5;
+//Console.WriteLine(num);
--- Day1/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ Day1/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52455b6f58f154018455dccbb18a553cb13b0d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

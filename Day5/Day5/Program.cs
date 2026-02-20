c1d08119e519d5f5b26acdcbd21a2f4a2577be4e
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-02-20 17:38:06 +0400

    day5 added

---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.Write("Bu Gün hava yağışlıdı: (H/Y)");
+//string input = Console.ReadLine();
+
+
+
+//if (input == "H")
+//    Console.WriteLine("Bəli Bu gün hava yağışlıdır!");
+//else
+//    Console.WriteLine("Xeyr, Bu gün hava günəşlidir!");
+
+
+
+//string result = (input == "H") ? "Bəli Bu gün hava yağışlıdır!" : "Xeyr, Bu gün hava günəşlidir";
+//Console.WriteLine(result );

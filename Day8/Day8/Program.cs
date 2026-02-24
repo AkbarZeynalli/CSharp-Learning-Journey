32d4c42024f5428dd56779b1747c9644beb45071
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-02-24 19:35:14 +0400

    day8 add

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day8
+{
+
+    public class Person()
+    {
+        public string Name;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Ekber";
+            Person p2 = p1;
+
+            p1.Name = "Selim";
+            Console.WriteLine("p1: " + p1.Name);
+            Console.WriteLine("P2: " + p2.Name);
+        }
+    }
+}

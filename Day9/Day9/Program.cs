a02966c9c85638a98373fb5f56f253e20a9a6dbc
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-02-25 15:25:44 +0400

    day9  add

---
+++ Day9/Day9/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            string message = "Hello EveryOne,how are you doing here ?";
+
+
+
+            //Contains:
+
+
+            //bool result = message.Contains("ekber");
+            //Console.WriteLine(result);
+
+            //bool result = message.Contains("Il",StringComparison.CurrentCultureIgnoreCase);
+            //Console.WriteLine(result);  
+
+
+
+
+            //StarsWith:
+
+
+
+            //Console.Write("Enter a word: ");
+            //string search  = Console.ReadLine();
+            //bool result = message.StartsWith(search, StringComparison.OrdinalIgnoreCase);
+
+            //if (result)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+
+
+
+            //EndWith:
+
+
+
+            //Console.Write("Enter a word: ");
+            //string search = Console.ReadLine();
+            //bool result = message.EndsWith(search,StringComparison.OrdinalIgnoreCase);
+            //if (result ) 
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+
+
+            //Replace: 
+            //string result = message.Replace("?", "!");
+            //Console.WriteLine(result);
+
+
+
+            //Split: 
+            //string simpleSplit = "Ekber,Zeynalli";
+            //string[] result  = simpleSplit.Split(",");
+            //Console.WriteLine(result[0]);
+            //Console.WriteLine(result[1]);
+
+
+
+            //Join:
+            //string[] names = {"Səlim","Ekberin","Qardaşıdır" };
+            //string result = String.Join(", ",names);
+            //Console.WriteLine(result);
+        }
+    }
+}

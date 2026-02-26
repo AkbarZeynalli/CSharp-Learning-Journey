21a20f079d1f9209b96707eb84d2fbcaae67c8aa
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-02-26 14:50:21 +0400

    day10 add and day9 modified

---
+++ Day10/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region StringMethods1
+            //string message = "Hello How are you doing here  ?";
+
+
+            //IndexOf:
+
+            //int result = message.IndexOf("H");
+            //Console.WriteLine(result);
+
+            //Substring: 
+            //string simpleSubString = message.Substring(5);
+            //Console.WriteLine(simpleSubString);
+
+
+            //Equals:
+            //string s1 = "Ekber";
+            //string s2 = "ekber";
+            //if (s1.Equals(s2,StringComparison.CurrentCultureIgnoreCase))
+            //    Console.WriteLine("Match");
+            //else
+            //    Console.WriteLine("Not Same"); 
+
+
+            //Format:
+            //string name = "Ekber";
+            //int age = 21;
+            //for (int i = 0; i < 3; i++)
+            //{
+            //string result = string.Format($"Name: {name}, Age: {age}");
+
+            //Console.WriteLine(result);
+            //}
+            #endregion
+
+            #region StringMethods2
+            //string txt1 = null;
+            //string txt2 = "";
+            //string txt3 = " ";
+            //Console.WriteLine(string.IsNullOrEmpty(txt1));
+            //Console.WriteLine(string.IsNullOrEmpty(txt2));
+            //Console.WriteLine(string.IsNullOrEmpty(txt3));
+
+            //Console.WriteLine(string.IsNullOrWhiteSpace(txt1));
+            //Console.WriteLine(string.IsNullOrWhiteSpace(txt2));
+            //Console.WriteLine(string.IsNullOrWhiteSpace(txt3));
+            //if (string.IsNullOrWhiteSpace(txt3))
+            //{
+            //    Console.WriteLine("Text3 is Empty");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Text3 is not Empty");
+            //}
+            #endregion
+        }
+    }
+}

c57e5b116fb07a58f7214956c1439f6c9d716454
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-01 23:01:24 +0400

    day11 add

---
+++ Day11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math 
+            //Math m = new Math();    
+            Console.WriteLine("PI Value: "+Math.PI);
+            Console.WriteLine("Euler's number: "+Math.E);
+
+            //Abs
+            int minusValue = -10;
+            int plusValue = Math.Abs(minusValue);
+            Console.WriteLine(plusValue);
+
+            //Pow
+            int powValue  = (int)Math.Pow(minusValue,2);
+            Console.WriteLine(powValue);
+        }
+    }
+}

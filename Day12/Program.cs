811f4b766c1f09ba5bb2dca9bead2f903f161213
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-05 23:45:58 +0400

    Today add Day12 and For Loop

---
+++ Day12/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+
+namespace Day12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //İstifadəçi bir N ədədi daxil etsin. for döngüsü ilə 1-dən N-ə qədər bütün ədədlərin cəmini hesabla.
+            //Console.Write("Enter number: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i < inputNumber; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine("1-dən "+inputNumber+"-ə qədər olan ədədlərin cəmi: "+sum);
+
+
+
+            //İstifadəçi iki ədəd daxil etsin: start və end.  for döngüsü ilə həmin intervaldakı bütün cüt ədədləri ekrana çıxar.
+            //Console.Write("Enter start Number: ");
+            //int startNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter End Number: ");
+            //int endNumber  = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for(int i = startNumber; i <= endNumber; i++)
+            //{
+            //    if (i %2 == 0)
+            //    {
+            //        Console.WriteLine($"Cüt ədədlər: {i}");
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine($"Cüt ədədlərin cəmi: {sum}");
+
+
+
+
+            //İstifadəçi N daxil etsin. for döngüsü ilə N! (factorial) hesabla.
+            //Console.Write("Enter number: ");
+            //int inputNumber= Convert.ToInt32(Console.ReadLine());
+            //int product = 1;
+            //for (int i = 1; i <= inputNumber; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    product *= i;
+            //}
+            //Console.WriteLine($"Daxil edilən ədədin factorialı: {product}");
+
+
+
+            //İstifadəçi bir ədəd daxil etsin. for və ya başqa üsulla rəqəmlərin cəmini tap.
+            //Console.Write("Enter Number: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //while (inputNumber > 0)
+            //{
+            //    sum += inputNumber % 10;
+            //    inputNumber /= 10;
+            //}
+            //Console.WriteLine($"Rəqəmlərin cəmi: {sum}");
+
+
+
+            //İstifadəçi N daxil etsin və for döngüsü ilə belə bir piramida çap et.
+            //Console.Write("Enter Number: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= inputNumber; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+    }
+}

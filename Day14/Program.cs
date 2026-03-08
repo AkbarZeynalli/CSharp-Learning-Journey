cd7932ae4ffd95fed34766bff115d9139922ddd1
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-08 10:16:04 +0400

    day14 add

---
+++ Day14/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Day14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //iç-içə döngülər
+
+            //Console.Write("Enter number: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= inputNumber; i++)
+            //{
+            //    for (int j = 1; j <= i ; j++)
+            //    {
+            //        Console.Write($"{j}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            Console.Write("Enter number: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                for (int j = 1; j <= inputNumber; j++)
+                {
+                    if (i==1||i==inputNumber ||j==1||j==inputNumber)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

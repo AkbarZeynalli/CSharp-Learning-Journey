e35bd3ee3899aa98512575b3ef89e1718fdfba47
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-10 01:11:26 +0400

    day15 add

---
+++ Day15/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Day15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach
+            //string[] carName = { "Mercedes", "Audi", "Pagani", "Toyota", "Ferrari", "Wolkswagen", "Honda","Hundai" };
+            //foreach (string item in carName)
+            //    Console.WriteLine(item);
+
+            //for(int i = 0;i<carName.Length;i++)
+            //    Console.WriteLine(carName[i]);
+            #endregion
+
+            #region List 
+            //List<string> countrys = new List<string>
+            //{
+            //    "Azerbaijan","Island","USA","Argentina","Brazil"
+            //};
+            //foreach(string country in countrys)
+            //    Console.WriteLine(country);
+            #endregion
+
+
+            #region Foreach Loop break and continue
+
+            #endregion
+
+        }
+    }
+}
+

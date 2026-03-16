0a6024d10fb961f00e443221d2370729f2c63f05
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-16 23:31:03 +0400

    day16 and 17 added

---
+++ Day16/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber  = random.Next(100,150);
+            double randomNumber2 = random.NextDouble()*10;
+            Console.WriteLine($"Random double number: {randomNumber2}");
+            Console.WriteLine(randomNumber);
+
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays
+            int[] arr1 = { 1, 2, 3 };
+            foreach(int item in arr1)
+                Console.WriteLine($"Item: {item}");
+
+
+            string info = "Ekber bu il her şeyi bacaracaq! hərkəsə və hərşeyə rəğmən";
+            foreach (int itm in info)
+                Console.WriteLine($"INFO: {itm}");
+        }
+    }
+}

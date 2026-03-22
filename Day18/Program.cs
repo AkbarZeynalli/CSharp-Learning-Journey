6ff33d90de4ce7ca5d6305bd5fee371fb10db450
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-22 23:07:52 +0400

    Day19 added

---
+++ Day18/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArr = { 10, 20, 30, };
+            foreach (int number in numberArr)
+            {
+                Console.WriteLine(number);
+            }
+
+            string[] fruits = { "apple", "strawberry", "melon", "watermelon", "orange", "grape" };
+            Console.WriteLine(fruits[1]);
+            fruits[1] = "banana";
+            Console.WriteLine(fruits[1]);
+            Console.WriteLine(fruits.Length);
+
+            foreach(string number in fruits)
+                 Console.WriteLine(number); 
+
+            for(int i = 0; i < fruits.Length; i++)
+                Console.WriteLine(fruits[i]);
+        }
+    }
+}
---
+++ Day19/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ARRAYS
+
+            //Console.WriteLine("Hello, World!");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] numbers2 = numbers[2..];
+            foreach(int number in numbers2) 
+                Console.WriteLine(number);
+
+
+            //Multidimensional Arrays:
+            int[,] multiNumbers = { { 1, 2, 3, }, { 4, 5, 6 }, { 7, 8, 9 } };
+            foreach(int number in multiNumbers)
+                Console.WriteLine(number);
+        }
+    }
+}

e86b59370a7ef9e3c96e479ee29ab3013b7d982c
AkbarZeynalli <akbarhz-af106@code.edu.az>
2026-03-26 11:18:58 +0400

    day22 add

---
+++ Day22/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Day22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sadə ədədlərin tapılması oyunu: 
+            Console.Write("Enter a number: ");
+            if (!int.TryParse(Console.ReadLine(), out int inputNumber))
+            {
+                Console.WriteLine("Invalid Input!");
+                return;
+            }
+
+            if (inputNumber <= 1)
+            {
+                Console.WriteLine("Not a prime number!");
+            }
+            
+            bool isPrime = true;
+
+            for (int i = 2; i <= Math.Sqrt(inputNumber); i++)
+            { 
+
+                if(inputNumber %i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPrime ? "Prime Number" : "Not a prime Number");
+
+        }
+    }
+}
